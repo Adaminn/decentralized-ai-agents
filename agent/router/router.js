9fb4b242167cf7185b69a8cc89a70104b9bcdcab
adam.siptak <adam.siptak@anthology.com>
2024-07-14 01:57:31 +0200

    fixed typo

--- agent/router/router.js
+++ agent/router/router.js
@@ -136,7 +136,7 @@ async function main() {
 
         const routerPrompt = "User submited this query: " + history + 
         ". The answer to this query from an assitent who is expert on this field is: " + output + 
-        ". Whit all this information, I can now reposnd to the users query. The response is: ";
+        ". With all this information, I can now reposnd to the users query. The response is: ";
 
         try {
             const response = await runInference(routerPrompt, 80);
